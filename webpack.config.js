3244f821fbc25a5c2c47d884f5a365000ed84d08
wucaixu <wucaixu@huobi.com>
2018-11-12 14:37:48 +0800

    be started

--- webpack.config.js
+++ webpack.config.js
@@ -43,11 +43,15 @@ module.exports = {
     mode: 'production',
     plugins: [
         new webpack.BannerPlugin('版权所有，翻版必究'),
-        new CleanWebpackPlugin('build/*.*', {
-            root: __dirname,
-            verbose: true,
-            dry: false
-        })
+        new HtmlWebpackPlugin({
+            template: __dirname + "/app/index.tmpl.html"//new 一个这个插件的实例，并传入相关的参数
+        }),
+        new webpack.HotModuleReplacementPlugin()//热加载插件
+        // new CleanWebpackPlugin('build/*.*', {
+        //     root: __dirname,
+        //     verbose: true,
+        //     dry: false
+        // })
     ],
 };
 

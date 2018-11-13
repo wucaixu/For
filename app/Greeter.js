7b5dbd36bdade3e793a5fc8d3354a728585a4d2c
wucaixu <wucaixu@huobi.com>
2018-11-13 10:17:00 +0800

    i

--- app/Greeter.js
+++ app/Greeter.js
@@ -6,11 +6,7 @@ class Greeter extends Component{
   render() {
     return (
       <div className={styles.root}> //使用cssModule添加类名的方法
-<<<<<<< HEAD
-        {config.greetText} 远端分支连接--c4---ant1
-=======
         {config.greetText} 12345612----c2-----c2
->>>>>>> c2
       </div>
     );
   }
